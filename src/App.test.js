e33bdba326ee1cca5b07a93e4c6163545d613822
Maaz <syed_maaz_hasan@yahoo.com>
2020-01-26 10:43:20 +0530

    added snapshot testing setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,13 @@
 import React from "react";
 import App from "./App";
 import { shallow } from "enzyme";
+import renderer from "react-test-renderer";
 
 it("renders without crashing", () => {
   const wrapper = shallow(<App></App>);
+});
+
+it("snapshot tetsing", () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
